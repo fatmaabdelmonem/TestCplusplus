7dcfbd0454f1a2a08bdc51a1bfc7b87d8e9e658a
fatmaabdelmonem <101889684+fatmaabdelmonem@users.noreply.github.com>
2022-05-12 00:38:53 +0200

    Add files via upload
    Number of character

--- main.cpp
+++ main.cpp
@@ -1,28 +1,31 @@
 
+#include<string.h>
 #include <iostream>
 #include <fstream>
-#include<string.h>
 using namespace std;
 
 int main()
 {
     char Line[80],fileName[25];
-    int counter;
+    int countt=0;
+    int len;
+    int i;
     ifstream fin;
-    cout<<" enter the name of file ";
+    cout<<" enter the name of file";
     cin>>fileName;
     fin.open(fileName,ios::in);
     if(!fin){
-        cout<<"file doesn`t exist  \n";
+            cout<<"file doesn`t exist  \n";
+            exit(0);}
+    cout<<"\n content of file is : \n";
 
-        exit(0);}
-        cout<<"\n content of file is : \n";
+    //count number of  characters
         while(fin.eof()==0){
-            fin.getline(Line,sizeof(Line));
-            ++counter;
-            cout<<Line;
-        }
-        fin.close();
-        cout<<"\n number of lines ="<<counter<<endl;
-        return 0;
-}
+        fin.getline(Line,sizeof(Line));
+        len=strlen(Line);
+        for(i=0;i<len;++i)
+             ++countt;
+        cout<<Line<<"\n";}
+
+    fin.close();
+    cout<< "\n number of characters ="<<countt<<endl;}
