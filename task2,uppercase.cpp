7eb4b69e8772b1a491e33db07cc5a1c4b04a6eca
fatmaabdelmonem <101889684+fatmaabdelmonem@users.noreply.github.com>
2022-05-13 21:20:25 +0200

    Add files via upload

---
+++ task2,uppercase.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+ char file[90];
+ cout<<"Enter file name: ";
+ cin>>file;
+ fstream myfile(file,ios::out | ios::in);
+ while(!myfile.eof()){
+    char ch;
+    myfile.get(ch);
+    int x= int(ch);
+
+    while(int (ch)<=122 && int(ch)>=95){
+
+
+        int y=int(ch)-32;
+        ch = char(y);
+
+    }
+ }
+return 0;}
